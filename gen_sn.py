dd798c2887a9d9ce4a3d74d66070f0823bcca06f
solutionLi <momo821123@outlook.com>
2020-11-16 09:04:08 +0900

    Modify

--- gen_sn.py
+++ gen_sn.py
@@ -1,5 +1,5 @@
 from itertools import cycle
-import base64
+import base64, re
 
 
 secret_data = "20201116"
@@ -35,21 +35,24 @@ def add_checksum(data):
     return data
 
 
+def check_checksum(data):
+    if len(re.findall("^[0-9]{10}$", s)) != 1:
+        return False
+
+
 def xor_crypt_string(data, encode = False): 
     if not encode:
         data = base64.decodebytes(bytes(data, 'utf-8')).decode("utf-8")
     
     xored = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(data, cycle(key)))
-
+    
     if encode:
         return base64.encodebytes(bytes(xored, 'utf-8')).decode("utf-8")
+    
     return xored
 
 
 def affine(data, encode = False):
-    # for b in bytes(data, 'utf-8'):
-        # print(chr((b * affine_multi + affine_add) % 128))
-    # return ""
     if encode:
         return "".join(chr(((b - 33) * affine_multi + affine_add) % 94 + 33) for b in bytes(data, 'utf-8'))
     else:
@@ -58,7 +61,6 @@ def affine(data, encode = False):
             d = b - 33 - affine_add
             while d % affine_multi != 0:
                 d += 94
-            # print(str(d) + "  " + str(affine_multi))
             result += chr(int(d / affine_multi) + 33)
         return result
 
@@ -66,20 +68,21 @@ def affine(data, encode = False):
 def encode(data):
     data = reverse(data)
     data = add_checksum(data)
-    print(data)
     data = xor_crypt_string(data, True)
-    print(data)
     data = affine(data[:-1], True)
-    print(data)
     return data
 
 
 def decode(data):
-    data = affine(data)
-    print(data)
-    data = xor_crypt_string(data, False)
-    print(data)
-
+    try:
+        data = affine(data)
+        data = xor_crypt_string(data, False)
+    except:
+        return ""
+    return data
 
 cipher_text = encode(secret_data)
+print(cipher_text)
 decode(cipher_text)
+a = decode("aaaaaaaaaaaaaaaa")
+print(a)
