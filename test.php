ff1385f2308aa0343d784a73db8655b1be62aa1f
solutionLi <momo821123@outlook.com>
2020-11-16 10:29:11 +0900

    Add test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+    <head>
+        <title>Genearte/test secure_code</title>
+        <meta charset = "UTF-8" />
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+    </head>
+    <body>
+        <h1 style="text-align-last: center;">Generate / Test Secure Code</h1>
+        <div style="justify-content:center; display:flex">
+            <div style="width:500px; text-align:center">
+            <form action="" method="post" class="form" role="form">
+                <div class="col-sm-12 data-field-col">
+                    <label for="to">Para</label>
+                    <input type="text" class="form-control" name="to" id="to">
+                </div>
+            </form>
+            </div>
+        </div>
+    
+    </body>
+</html>
